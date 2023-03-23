6ed844538b48b0940ff9d378851f676b9801689c
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-03-23 17:29:55 +0900

    회원가입구현

--- src/main/java/com/sparta/springcore/model/Product.java
+++ src/main/java/com/sparta/springcore/model/Product.java
@@ -1,10 +1,11 @@
 package com.sparta.springcore.model;
 
 import com.sparta.springcore.dto.ProductRequestDto;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 
 @Setter
