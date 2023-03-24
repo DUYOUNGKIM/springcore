4cc8fff3bca6469dbb5f2c05067d10d5ec6666d5
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-03-25 00:33:17 +0900

    mvc테스트 구현

---
+++ src/test/java/com/sparta/springcore/mvc/MockSpringSecurityFilter.java
@@ -0,0 +1,26 @@
+package com.sparta.springcore.mvc;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+//시큐리티의 가짜 객체를 만들기 위해 필요하다
+public class MockSpringSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        SecurityContextHolder.getContext()
+                .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
