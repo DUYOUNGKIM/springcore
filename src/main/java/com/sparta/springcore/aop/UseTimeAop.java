758ddf71518dc0be237cc2f2e69129c3b9cfa390
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-03-30 18:47:39 +0900

    golbal excption 처리 및 api 호출횟수 생성

--- src/main/java/com/sparta/springcore/aop/UseTimeAop.java
+++ src/main/java/com/sparta/springcore/aop/UseTimeAop.java
@@ -35,6 +35,7 @@ public class UseTimeAop {
 // 수행시간 = 종료 시간 - 시작 시간
             long runTime = endTime - startTime;
 
+
 // 로그인 회원이 없는 경우, 수행시간 기록하지 않음
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth != null && auth.getPrincipal().getClass() == UserDetailsImpl.class) {
